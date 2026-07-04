83d8497759adc7aa880c03d5b57d3d91d6508645
seshank5134 <seshank5134@gmail.com>
2026-07-04 22:21:57 +0530

    Time: 1 ms (99.97%), Space: 46.4 MB (67.74%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+         int[] arr = new int[26];
+        for (char c : magazine.toCharArray()) {// with asci calcluation save every thing from magzine 
+            arr[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {// going to all elements and checking if magzine is not in ransome 
+            if (--arr[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;     
+    }
+}
