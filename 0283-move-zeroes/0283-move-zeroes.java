a2432c176b6351da887f97f85104094c82e97e39
seshank5134 <seshank5134@gmail.com>
2026-07-04 22:20:04 +0530

    Time: 1 ms (99.83%), Space: 47.9 MB (37.94%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) { // sort zeo to end   by first half upto l 
+         int l = 0; int r = 0;
+        for (int i = 0; i<nums.length;i++){
+            if (nums[i]!=0){
+                nums[l] = nums[i];
+                l++;
+            }
+        }
+        for (int i = l; i<nums.length;i++){// save zeros from l onwards 
+            nums[i] = 0;
+        }
+    }
+}
