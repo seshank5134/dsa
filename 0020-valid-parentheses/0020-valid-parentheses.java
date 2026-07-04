e224f3d0e0ecf5aecb3abcaf978d9f52c42de5fc
seshank5134 <seshank5134@gmail.com>
2026-07-04 22:23:27 +0530

    Time: 3 ms (87.24%), Space: 43.5 MB (26.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+         Stack<Character> stack = new Stack<>();// stack technique 
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {// start checking
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) { // end checking for paraentheses 
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
