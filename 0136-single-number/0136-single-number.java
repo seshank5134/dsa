3653166cb414b44938c3490722075994a31a2f68
seshank5134 <seshank5134@gmail.com>
2026-07-10 22:10:45 +0530

    Time: 1 ms (99.97%), Space: 46.7 MB (92.4%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,4 +1,9 @@
-class Solution {
+class Solution {//If your input array is nums = [4, 1, 2, 1, 2]:
+//Initial state: result = 0Step 1: 0 ^ 4  -> 4
+//Step 2: 4 ^ 1 \(\rightarrow \) 4 ^ 1
+//Step 3: 4 ^ 1 ^ 2 -> 4 ^ 1 ^ 2
+//Step 4: 4 ^ 1 ^ 2 ^ 1 ->(The 1s cancel out ) 4 ^ 2
+//Step 5: 4 ^ 2 ^ 2  -> (The 2s cancel out)  4
     public int singleNumber(int[] nums) {
         int result = 0;
         for (int n : nums) {// direct xor option applicablers here 
