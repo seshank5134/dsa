cf3d362bde19869d73f29f485dc32bbe06996f1a
seshank5134 <seshank5134@gmail.com>
2026-07-13 20:31:55 +0530

    Time: 0 ms (100%), Space: 47.4 MB (48.62%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {//  have to get non existing within the size of array 
+    public int missingNumber(int[] nums) {
+       int n= nums.length;
+       int expectedSum = n * (n + 1) / 2;
+       int sum = 0;
+       for ( int x : nums){
+            sum += x;// sum of all n* n+1/2 and will remove the actual and expected 
+       }
+       return expectedSum - sum;// here we remove by complement logic 
+            
+        }
+    }
