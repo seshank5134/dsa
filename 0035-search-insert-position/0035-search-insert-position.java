5272605e07a7b8eb79b0bfcd1a8a4179c1453a71
seshank5134 <seshank5134@gmail.com>
2026-07-14 22:04:54 +0530

    Time: 0 ms (100%), Space: 44.2 MB (98.23%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {int n = nums.length;
+         int left = 0 ; int right = n-1 ;int mid =0 ; 
+        while ( left <= right ){// need to check until left > right there is chance of element to be presented in the sorted array 
+            mid = left+(right-left)/2;// to avoid overflow 
+            if (nums[mid] == target)
+    return mid;
+
+else if (nums[mid] < target)
+    left = mid + 1;// less than req discard left side elements and here we are doing mid +1 because we already go through mid right no need to recheck of it 
+
+else
+    right = mid - 1;// if high than req then discard right side part also do one less of mid as we already checked the mid so mid-1 
+        }
+        return left; 
+    }
+}
