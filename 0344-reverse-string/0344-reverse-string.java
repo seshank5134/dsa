99d0b3c5e3052d9366a5b9b4b31e2b9a0a35e0e2
seshank5134 <seshank5134@gmail.com>
2026-07-17 21:43:25 +0530

    Time: 0 ms (100%), Space: 48.6 MB (10.87%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {// classic revsre wiith new temp character 
     public void reverseString(char[] s) {
        int l = 0; int r = s.length-1; //We don't need another array because the characters can be swapped in-place, reducing space complexity from O(n) to O(1)
         for ( int i = 0; i< s.length/2;i++){
