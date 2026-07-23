282700e62c9c91e5073d32c5062abc57acacbfb7
seshank5134 <seshank5134@gmail.com>
2026-07-23 22:40:02 +0530

    Time: 0 ms (100%), Space: 44 MB (27.64%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,13 +1,14 @@
-class Solution {
+class Solution {// typical binary problem with a rotation 
     public int search(int[] nums, int target) {int left = 0 ; int right = nums.length-1;
-        while (left <= right) {
+        while (left <= right) {// same logic for left and right 
 
     int mid = left + (right - left) / 2;
 
     if (nums[mid] == target)
         return mid;
+        // here to we need to check whether mid is in sort or not so we can identify as everything is sorterd just rotated 
 
-    // Left half is sorted
+    // Left half is sorted(whether the  mid is in left and left side is sorted )
     if (nums[left] <= nums[mid]) {
 
         if (nums[left] <= target && target < nums[mid]) {
@@ -17,7 +18,7 @@ class Solution {
         }
 
     }
-    // Right half is sorted
+    // Right half is sorted( checking whethermid is right andd right as sorted )
     else {
 
         if (nums[mid] < target && target <= nums[right]) {
