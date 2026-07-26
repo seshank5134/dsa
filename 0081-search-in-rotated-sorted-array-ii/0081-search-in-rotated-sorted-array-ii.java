0f45bd610572a8bcd04be07900600e645241189a
seshank5134 <seshank5134@gmail.com>
2026-07-26 22:48:00 +0530

    Time: 0 ms (100%), Space: 44.9 MB (82.61%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int left = 0; 
         int right = nums.length-1;
         while (left<=right){
-            int mid=left +(right-left)/2;
+            int mid=left +(right-left)/2;//We first check whether mid is the target. If not, then we check duplicates and discard them.
                if (nums[mid] == target){
         return true ;}
             if(nums[left]==nums[mid] && nums[mid]== nums[right]) {
