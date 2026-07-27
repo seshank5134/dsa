78a4fd83a9650bd47d5a48dcca6d0fc8853e154d
seshank5134 <seshank5134@gmail.com>
2026-07-27 23:00:14 +0530

    Time: 10 ms (55%), Space: 48.5 MB (11.66%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,13 +1,13 @@
-class Solution {
+class Solution {// binary search 
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;
-         int right = Arrays.stream(piles).max().getAsInt(); // to get max of piles array
-         int ans = right ;
+        int left = 1;// minimum would be eating of bananas is 1 per hour so left be 1
+         int right = Arrays.stream(piles).max().getAsInt(); // to get max of piles array  this is method for getting maximum number from the array pile which is arrays.stream().max().getAsIn().
+         int ans = right ; // as max may become answer if eating per hour least is 1 ( like 30 max minimum eating is 1 then answer is 30 )
         while(left<=right){
              int mid = left + (right - left) / 2; 
              long hours =0;
             for (int pile : piles ){ 
-             hours += (pile + mid - 1) / mid;// maths ceil formula 
+             hours += (pile + mid - 1) / mid;//  instead of  maths.ceil()maths ceil formula 
                                     }
 
              if(hours<=h){
