e75492741b1d44b358dd763eba7b598520811155
seshank5134 <seshank5134@gmail.com>
2026-07-31 22:49:38 +0530

    Time: 15 ms (79.18%), Space: 93.8 MB (56.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-      /* intial idea brute answer complexity O(n^2)
+      /*
         for (int i = 0; i < nums.length; i++) {
 
         for (int j = i + 1; j < nums.length; j++) {
@@ -12,8 +12,6 @@ class Solution {
     }
 
     return false;*/
-    /*second optimization  O( n )
-    
     Set<Integer> set = new HashSet<> () ;
         for( int num: nums ){
             if ( set.contains(num)) {
@@ -23,15 +21,6 @@ class Solution {
                 set.add(num) ;
             }
         }
-        return false ;*/
-        // smart add into set 
- Set<Integer> set = new HashSet<>();
-    for (int num : nums) {
-        if (!set.add(num)) {
-            return true;
-        }
-    }
-
-    return false;
+        return false ;
     }
 }
