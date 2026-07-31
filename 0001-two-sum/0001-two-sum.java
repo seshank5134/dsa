8371145d25bc6a0e252ea9fcfda2112f03f7c22c
seshank5134 <seshank5134@gmail.com>
2026-07-31 22:53:57 +0530

    Time: 2 ms (99.31%), Space: 47.2 MB (23.59%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      /*  for (int i = 0; i < nums.length; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return new int[] {i, j};
+            }
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");*/
+
+        // ikada element vundha ledha easy ga check cheyataniki hash map ( have u seen before )
+
+
+    Map<Integer, Integer> map = new HashMap<>();  // already vunna elements check cheyataniki easy ga hash map use avuthundhi 
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i]; // complement chesthe easy ga next sum kanukotaniki 
+        if (map.containsKey(complement)) {
+            return new int[] {map.get(complement), i};
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+    }
+}
