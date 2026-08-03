aa4a7aec71c4b5220cc93e238db187f0aca6cac1
seshank5134 <seshank5134@gmail.com>
2026-08-03 20:42:30 +0530

    Time: 24 ms (76.44%), Space: 49.3 MB (5.61%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,23 +1,23 @@
-class Solution {
+class Solution {// have to get numbers which gets target so we need to check frequencies as repeated possible so here hashmap suitable 
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-map.put(0, 1);
+map.put(0, 1); // to start from 1 we have make sure that if nothing then 0 is present 
 
 int sum = 0;
 int count = 0;
 
 for (int num : nums) {
-
+// going through each and every element
     sum += num;
 
-    if (map.containsKey(sum - k)) {
+    if (map.containsKey(sum - k)) {// if map contains themn will all the count 
         count += map.get(sum - k);
     }
 
-    map.put(sum, map.getOrDefault(sum, 0) + 1);
+    map.put(sum, map.getOrDefault(sum, 0) + 1); // will retrive that after check everytime 
 }
 
-return count;
+return count;// our requested from question is count of integers
     }
 }
