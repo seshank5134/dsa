9a6c5f266f6afa50b1925a1bec29e9a12e696334
seshank5134 <seshank5134@gmail.com>
2026-08-28 23:33:42 +0530

    Time: 5 ms (31.97%), Space: 69.2 MB (94.79%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,5 +1,5 @@
 class Solution {
-    public double findMaxAverage(int[] nums, int k) {
+    public double findMaxAverage(int[] nums, int k) {// n time and 1 space
         double max=0;
         for (int i = 0; i < k; i++) { // this window sliding problem 
             max+=nums[i];
